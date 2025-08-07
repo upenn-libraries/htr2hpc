40599e19cbf2cc7483055c958c4683d0d23f1c29
Doug Emery <emeryr@upenn.edu>
2025-08-07 16:02:38 -0400

    Update local_settings.py

--- src/htr2hpc/urls.py
+++ src/htr2hpc/urls.py
@@ -2,9 +2,9 @@ from django.urls import include, path
 
 from escriptorium.urls import urlpatterns
 
-from htr2hpc.views import remote_user_setup
+# from htr2hpc.views import remote_user_setup
 
-urlpatterns += [
-    (path("accounts/", include("pucas.cas_urls"))),
-    path("profile/hpc-setup/", remote_user_setup, name="hpc-setup"),
-]
+# urlpatterns += [
+#     (path("accounts/", include("pucas.cas_urls"))),
+#     path("profile/hpc-setup/", remote_user_setup, name="hpc-setup"),
+# ]
